0d8fa52bfd97329f6dbd5bc6a208988c2ca50229
nmetz <info@normanmetz.com>
2023-10-03 10:47:24 +0200

    feat(header): set spacings

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,6 @@
-const container = document.querySelector('.portfolio__heading');
+const container = document.querySelector('.portfolio__header');
 const button = document.querySelector('.portfolio__button');
 
 button.addEventListener('click', () => {
-	container.classList.toggle('portfolio__heading--collapsed')
+	container.classList.toggle('portfolio__header--collapsed')
 })
