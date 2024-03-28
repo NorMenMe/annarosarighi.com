ee3834b158c18376c5cb0187018275d681c09a10
nmetz <info@normanmetz.com>
2024-03-28 09:17:55 +0100

    feat(header): add lazer loader animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-
+import { LazerLoader } from './lazer-loader.js';
 
 const container = document.querySelector(".portfolio__header");
 const button = document.querySelector(".portfolio__button");
@@ -7,3 +7,22 @@ button.addEventListener("click", () => {
   container.classList.toggle("portfolio__header--collapsed");
 });
 
+
+
+// // Into-Viewport Animation
+const allLists = document.querySelectorAll('.portfolio__header');
+
+if (allLists.length) {
+  const Lazer = new LazerLoader({
+    targets: allLists,
+    config: {
+      rootMargin: '0px',
+      threshold: 0.01,
+    },
+    cb: (target, isAbove) => {
+      !isAbove && target.classList.add('ani-in');
+    },
+  });
+
+  Lazer.init();
+}
--- src/logic.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.portfolio__header');
-const button = document.querySelector('.portfolio__button');
-
-button.addEventListener('click', () => {
-	container.classList.toggle('portfolio__header--collapsed')
-})
