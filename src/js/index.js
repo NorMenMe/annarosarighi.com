01fad912e2c5d6cb2f4b423dfa5e5e9e6f8d35f9
nmetz <info@normanmetz.com>
2024-11-23 11:40:16 +0100

    fix(header): add a link-button & fix overfloating elements

--- src/js/index.js
+++ src/js/index.js
@@ -24,11 +24,15 @@ if (allLists.length) {
 const buttons = document.querySelectorAll('.header__button');
 
 buttons.length && buttons.forEach( button => {
-  button.addEventListener('click', () => {
-    let isCollapsed = button.getAttribute('aria-expanded') === 'true';
+  button.addEventListener('click', (event) => {
+    const currentClickedButton = event.currentTarget;
+    const container = currentClickedButton.closest('.header');
+
+    let isCollapsed = currentClickedButton.getAttribute('aria-expanded') === 'true';
     isCollapsed = !isCollapsed;
-    button.setAttribute('aria-expanded', isCollapsed);
-    isCollapsed ? button.parentElement.classList.add('is-collapsed') : button.parentElement.classList.remove('is-collapsed');
+    currentClickedButton.setAttribute('aria-expanded', isCollapsed);
+
+    isCollapsed ? container.classList.add('is-collapsed') : container.classList.remove('is-collapsed');
   })
 })
 
