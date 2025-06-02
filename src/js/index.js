e8b66641567cf02266fd9197ce44ec783f03a69b
nmetz <info@normanmetz.com>
2025-06-02 11:53:13 +0200

    feat(back-to-top): add feature back-to-top button

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import { LazerLoader } from './lazer-loader.js';
 import { TiltImage } from './tilt-image.js';
 
 //  Into-Viewport Animation
+
 const allLists = document.querySelectorAll('.portfolio__header');
 
 if (allLists.length) {
@@ -20,11 +21,6 @@ if (allLists.length) {
 }
 
 // Dropdown 
-
-// listen on mousedown
-// on fire change the cursor style from grab to grabbing
- // on button listen on mousedown
-  // if mousedown change cursor
 
 const buttons = document.querySelectorAll('.header__button');
 
@@ -56,3 +52,55 @@ buttons.length && buttons.forEach( button => {
 })
 
 TiltImage();
+
+// Back-to-top Button
+
+const BREAKPOINTDESKTOP = 1200;
+const TRESHOLD = -1500;
+const backToTop = document.querySelector('.back-to-top__button');
+
+
+const hideBackToTop = (params) => {
+  backToTop.classList.add('hidden');
+}
+
+const showBackToTop = (params) => {
+    backToTop.classList.remove('hidden');
+}
+
+const handleOnScroll = ( ) => {
+  const targetElement = document.querySelector('body');
+
+  if (targetElement) {
+    const distanceFromWindowTop = targetElement.getBoundingClientRect().y;
+
+    if (Math.sign(distanceFromWindowTop) === -1 && distanceFromWindowTop < TRESHOLD) {
+      showBackToTop();
+    } else {
+      hideBackToTop();
+    }
+  }
+}
+
+const scrollOnClick = () => {
+  window.scrollTo({
+  top: 0,
+  behavior: "smooth",
+  });
+}
+
+const bindEvents = ( ) => {
+  document.addEventListener('scroll', handleOnScroll);
+  backToTop.addEventListener('click', scrollOnClick);
+}
+
+const hideOnDesktop = ( ) => {
+ window.innerWidth >= BREAKPOINTDESKTOP 
+    ? hideBackToTop()
+    : '';
+}
+
+if ( backToTop ) {
+  hideOnDesktop();
+  bindEvents();
+}
